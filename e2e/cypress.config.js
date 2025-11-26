a77917b8e2a6e01e92b1287a24227a4f063bcdab
Levan Tsivadze <levani.tsivadze@gmail.com>
2025-11-26 23:47:23 +0400

    change cypress config

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: 'e2e/cypress/support/e2e.js',
-    specPattern: 'e2e/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
