4b452af4281534faf71e51e1ba2aab6451d28b12
Levan Tsivadze <levani.tsivadze@gmail.com>
2025-11-26 22:39:29 +0400

    include cypress e2e

---
+++ e2e/cypress/e2e/pokedex-name.cy.js
@@ -0,0 +1,10 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    // eslint-disable-next-line no-undef
+    cy.visit('http://localhost:3000')
+    // eslint-disable-next-line no-undef
+    cy.contains('ivysaur')
+    // eslint-disable-next-line no-undef
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
