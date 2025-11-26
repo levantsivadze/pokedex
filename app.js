0ce4e56d2ac20b0053ff710fa91b149ad0b7ad73
Levan Tsivadze <levani.tsivadze@gmail.com>
2025-11-26 21:10:25 +0400

    fail lint

--- app.js
+++ app.js
@@ -7,6 +7,5 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
